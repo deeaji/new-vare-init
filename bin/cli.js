35c8437f397eead8003d79981cf78f3bbd2d4630
deeaji <deeajibola@gmail.com>
2023-04-17 15:08:35 -0400

    update repo

--- bin/cli.js
+++ bin/cli.js
@@ -1,28 +1,49 @@
-#!/usr/bin/env node
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
-const { exeSync } = require('child_process');
+if (process.argv.length < 3) {
+  console.log("my boilerplate");
+  console.log("For example :");
+  console.log("    npx create-my-boilerplate my-app");
+  process.exit(1);
+}
 
-const runCommand = command => {
-  try {
-    exeSync(`${command}`, { stdio: 'inherit' });
-  } catch (e) {
-    console.log(`Failed to execute ${command}`, e);
-    return false;
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const git_repo = "https://github.com/deeaji/test-vare-init";
+
+try {
+  fs.mkdirSync(projectPath);
+} catch (err) {
+  if (err.code === "EEXIST") {
+    console.log(
+      `The file ${projectName} already exist in the current directory, please give it another name.`
+    );
+  } else {
+    console.log(err);
   }
-  return true;
-};
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --dept 1 http://github.com/hhimanshu/react-starter ${repoName}`;
-const installDepCommand = `cd ${repoName} && ${repoName}`;
+  process.exit(1);
+}
 
-console.log(`cloning the repository with ${repoName}`);
-const checkout = runCommand(gitCheckoutCommand);
+async function main() {
+  try {
+    console.log("Downloading files...");
+    execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
 
-if (!checkout) process.exit(code);
+    process.chdir(projectPath);
 
-console.log(`Installing dependencies for ${repoName}`);
-const installDep = runCommand(installDepCommand);
+    console.log("Installing dependencies...");
+    execSync("npm install");
 
-if (!installDep) process.exit(code);
+    console.log("Removing useless files");
+    execSync("npx rimraf ./.git");
+    fs.rmdirSync(path.join(projectPath, "bin"), { recursive: true });
 
-console.log('Congratulations you are ready. Follow the command to start');
+    console.log("The installation is done, this is ready to use !");
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
