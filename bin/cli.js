eb93d99a44ab6c5efc39e184bab19dcdfc4b5a19
deeaji <deeajibola@gmail.com>
2023-04-18 07:19:23 -0400

    update repo v2

--- bin/cli.js
+++ bin/cli.js
@@ -12,7 +12,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/unegbuclinton/reacr-tsx-boilerplate.git";
+const git_repo = "https://github.com/deeaji/test-vare-init.git";
 
 try {
   fs.mkdirSync(projectPath);
